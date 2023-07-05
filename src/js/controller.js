a25c82e46ed9b15dd1f17b28947865ce9b7881f1
Patrox255 <patryksz385@gmail.com>
2023-07-05 22:25:33 +0200

    Initial commit

---
+++ src/js/controller.js
@@ -0,0 +1,110 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import bookmarksView from './views/bookmarksView.js';
+import paginationView from './views/paginationView.js';
+import addRecipeView from './views/addRecipeView.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+
+// import icons from '../img/icons.svg';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// https://forkify-api.herokuapp.com/v2
+// My API Key: 9e05fece-7183-4902-a81d-59162ae956d4
+
+///////////////////////////////////////
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  const id = window.location.hash.slice(1);
+  if (!id) return;
+
+  try {
+    recipeView.renderSpinner();
+
+    resultsView.update(model.getSearchResultsPage());
+    await model.loadRecipe(id);
+
+    recipeView.render(model.state.recipe);
+
+    bookmarksView.update(model.state.bookmarks);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  const query = searchView.getQuery();
+  if (!query) return;
+  try {
+    resultsView.renderSpinner();
+    await model.loadSearchResults(query);
+    resultsView.render(model.getSearchResultsPage());
+
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  model.updateServings(newServings);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  recipeView.update(model.state.recipe);
+
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    addRecipeView.renderSpinner();
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    recipeView.render(model.state.recipe);
+
+    addRecipeView._toggleWindow;
+    addRecipeView.renderMessage();
+
+    bookmarksView.render(model.state.bookmarks);
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+  } catch (err) {
+    console.log('💥💥' + err.message);
+    addRecipeView.renderError(err);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Welcome');
+};
+init();
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,64 @@
+import icons from 'url:../../img/icons.svg';
+import View from './View';
+import { RES_PER_PAGE } from '../config';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+
+      if (!btn) return;
+
+      console.log(+btn.dataset.goto);
+      handler(+btn.dataset.goto);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    console.log(this._data);
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resPerPage
+    );
+    console.log(numPages);
+    const btnPrev = `
+        <button class="btn--inline pagination__btn--prev" data-goto="${
+          curPage - 1
+        }">
+        <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${curPage - 1}</span>
+        </button>
+    `;
+    const btnNext = `
+        <button class="btn--inline pagination__btn--next" data-goto="${
+          curPage + 1
+        }">
+        <span>Page ${curPage + 1}</span>
+        <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+        </button>
+    `;
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return btnNext;
+    }
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return btnPrev;
+    }
+    // Other page
+    if (curPage > 1 && curPage < numPages) {
+      return btnPrev + btnNext;
+    }
+    // Page 1, and there aren't other pages
+    return '';
+  }
+}
+
+export default new PaginationView();
