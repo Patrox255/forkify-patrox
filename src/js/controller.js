37870ee641d87b011d3ddba4a67509d5f680b4f9
Patrox255 <patryksz385@gmail.com>
2023-07-05 22:34:26 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -97,6 +97,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -105,6 +109,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
